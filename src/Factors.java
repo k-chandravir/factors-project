eb77284430b0b5f7e495e70f152936885e65685a
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-03-23 15:37:56 +0530

    Add. Included The Logic For Finding Factors

--- src/Factors.java
+++ src/Factors.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Factors {
+
+    public static void factors(int numberForFactors) {
+
+        for (int factor = 1; factor <= numberForFactors; factor++) {
+            if (numberForFactors % factor == 0) {
+                System.out.println(factor);
+            }
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number please: ");
+
+        int numberForFactors = input.nextInt();
+        factors(numberForFactors);
     }
 }
